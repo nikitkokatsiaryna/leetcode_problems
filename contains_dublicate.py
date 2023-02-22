fe2034af1d69c169e9bc238e98d166447930a209
Katerina Nikitko <ekaterina.nikitka@1000geeks.com>
2023-02-22 13:09:46 -0400

    add task "Check dublitates in array"

--- contains_dublicate.py
+++ contains_dublicate.py
@@ -1,3 +1,18 @@
+# Given an integer array nums, return true if any value appears at least twice in the array,
+# and return false if every element is distinct.
+
+# Example 1:
+# Input: nums = [1,2,3,1]
+# Output: true
+
+# Example 2:
+# Input: nums = [1,2,3,4]
+# Output: false
+
+# Example 3:
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+
 from typing import List
 
 
@@ -14,6 +29,6 @@ class Solution:
 
 
 solution = Solution()
-solution_result = solution.containsDuplicate([3, 3, 7, 7, 10, 11, 11])
+solution_result = solution.containsDuplicate([3, 3, 7, 7, 10, 11, 11]) # true
 # solution_result = solution.containsDuplicate([2, 14, 18, 22, 23])
 print(solution_result)
